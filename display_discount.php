77594ed7ed320b3cff9b4ff66d518cd4820cb34a
nicole a raeder <nar28@njit.edu>
2017-04-19 11:51:15 -0400

    added statements for product description, list price, and discount percentage

--- display_discount.php
+++ display_discount.php
@@ -1,3 +1,18 @@
+<?php
+$product_description = $_POST['product_description'];
+$list_price = $_POST['list_price'];
+$discount_percent = $_POST['discount_percent'];
+
+$discount = $list_price * $discount_percent * .01;
+$discount_price = $list_price - $discount;
+
+$list_price_formatted = "$".number_format($list_price, 2);
+$discount_percent_formatted = $discount_percent."%";
+$discount_formatted = "$".number_format($discount, 2);
+$discount_price_formatted = "$".number_format($discount_price, 2);
+$product_description_escaped = htmlspecialchars($product_description);
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
